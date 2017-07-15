61902b0ba8638ab3708acd0637c40149f0729076
Mark Kampe <mark.kampe@gmail.com>
2017-07-15 15:19:50 -0700

    Parameters: eliminate obsolete parms, add arg parser

--- src/Parameters.java
+++ src/Parameters.java
@@ -6,16 +6,17 @@
  */
  
  public class Parameters {
+	// display parameters
+	public int height = 800;		// screen height
+	public int width = 800;			// screen width
 	
 	// map generation parameters
 	public double x_extent = 1.0;	// Xmax - Xmin
 	public double y_extent = 1.0;	// Ymax - Ymin
 	public int improvements = 1;	// number of smoothing iterations
-	public int points = 200;		// desired number of grid points
+	public int points = 2048;		// desired number of grid points
 	
 	// diagnostic options
-	public boolean show_points = false;	// display original and improved points
-	public boolean show_grid = true;	// display final grid
 	public int debug_level = 1;			// level of verbosity
 	
 	private static Parameters singleton = null;
@@ -25,5 +26,17 @@
 		if (singleton == null)
 			singleton = new Parameters();
 		return singleton;
+	}
+	
+	/**
+	 * parse a switch specification and set options accordingly
+	 * 
+	 * @param arg	String to be parsed (less the switch character)
+	 */
+	public void parseSwitch( String arg ) {
+		// figure out what the option is
+		char c = arg.charAt(0);
+		switch (c) {
+		}
 	}
 }

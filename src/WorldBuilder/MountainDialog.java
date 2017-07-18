496ad0be6f5414adb1dc54cad3359b2924662f52
Mark Kampe <mark.kampe@gmail.com>
2017-07-17 17:21:50 -0700

    MountainDialog - WIP

--- src/WorldBuilder/MountainDialog.java
+++ src/WorldBuilder/MountainDialog.java
@@ -1,33 +1,16 @@
 package WorldBuilder;
 
-import java.awt.Color;
-import java.awt.Container;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
+import java.awt.*;
+import java.awt.event.*;
 
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JSlider;
-import javax.swing.WindowConstants;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-
+import javax.swing.*;
+import javax.swing.event.*;
 
 /**
  * SlopeDialog allows the user to choose an axis and inclination to
- * cause a uniform slope to the entire map.
+ * cause a uniform slope to the entire map.  
  */
-public class MountainDialog extends JFrame implements MouseMotionListener, MouseListener, ActionListener, ChangeListener, WindowListener {	
+public class MountainDialog extends JFrame implements ActionListener, ChangeListener, MouseListener, MouseMotionListener, WindowListener {	
 	private Map map;
 	private Mesh oldMesh;
 	private Mesh newMesh;
@@ -38,9 +21,8 @@ public class MountainDialog extends JFrame implements MouseMotionListener, Mouse
 	private JButton accept;
 	private JButton cancel;
 	
-	private int select_x;	// X coordinate of press
-	private int select_y;	// Y coordinate of press
-	private int select_type;	// type of selection in progress
+	private boolean selecting;		// selection in progress
+	private int x_start, x_end, y_start, y_end;		// selection start/end coordinates
 	
 	private static final int BORDER_WIDTH = 5;
 	
@@ -51,44 +33,140 @@ public class MountainDialog extends JFrame implements MouseMotionListener, Mouse
 		this.map = map;
 		this.oldMesh = map.getMesh();
 		this.newMesh = new Mesh(this.oldMesh);
+		map.setMesh(newMesh);
 		this.parms = Parameters.getInstance();
-		
-		// register to receive mouse events (for selection)
-		map.addMouseMotionListener(this);
-		map.addMouseListener(this);
 		
 		// create the dialog box
 		Container mainPane = getContentPane();
 		((JComponent) mainPane).setBorder(BorderFactory.createMatteBorder(BORDER_WIDTH, BORDER_WIDTH, BORDER_WIDTH, BORDER_WIDTH, Color.LIGHT_GRAY));
-		setTitle("Define Mountain/Range");
+		setTitle("Add Mountain(s)");
 		addWindowListener( this );
 		setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
 		
-		// create the widgets
-		altitude = new JSlider();
-		altitude.addChangeListener(this);
-		diameter = new JSlider();
-		diameter.addChangeListener(this);
+		// create the basic widgets
+		Font fontSmall = new Font("Serif", Font.ITALIC, 10);
+		Font fontLarge = new Font("Serif", Font.ITALIC, 15);
 		accept = new JButton("ACCEPT");
-		accept.addActionListener(this);
 		cancel = new JButton("CANCEL");
-		cancel.addActionListener(this);
 		
-		// assemble them all into a dialog box
-		JPanel controls = new JPanel(new GridLayout(3,2));
-		controls.add(altitude);
-		controls.add(diameter);
-		controls.add(new JLabel("Height", JLabel.CENTER));
-		controls.add(new JLabel("Diameter", JLabel.CENTER));
-		controls.add(cancel);
-		controls.add(accept);
-		mainPane.add(controls);
+		altitude = new JSlider(JSlider.HORIZONTAL, -5000, 5000, 0);
+		altitude.setMajorTickSpacing(5000);
+		altitude.setMinorTickSpacing(1000);
+		altitude.setFont(fontSmall);
+		altitude.setPaintTicks(true);
+		altitude.setPaintLabels(true);
+		JLabel altitudeLabel = new JLabel("Altitude", JLabel.CENTER);
+		altitudeLabel.setFont(fontLarge);
+
+		diameter = new JSlider(JSlider.HORIZONTAL, 0, 500, 50);
+		diameter.setMajorTickSpacing(100);
+		diameter.setMinorTickSpacing(50);
+		diameter.setFont(fontSmall);
+		diameter.setPaintTicks(true);
+		diameter.setPaintLabels(true);
+		JLabel diameterLabel = new JLabel("Diameter", JLabel.CENTER);
+		diameterLabel.setFont(fontLarge);
 		
-		// display the dialog box
+		/*
+		 * Pack them into:
+		 * 		a vertical Box layout containing sliders and buttons
+		 * 		sliders are a 1x2 grid layout
+		 * 			each being a vertical Box w/label and slider
+		 * 		buttons a horizontal Box layout
+		 */
+		JPanel altPanel = new JPanel();
+		altPanel.setLayout(new BoxLayout(altPanel, BoxLayout.PAGE_AXIS));
+		altPanel.add(altitudeLabel);
+		altPanel.add(altitude);
+		
+		JPanel diaPanel = new JPanel();
+		diaPanel.setLayout(new BoxLayout(diaPanel, BoxLayout.PAGE_AXIS));
+		diaPanel.add(diameterLabel);
+		diaPanel.add(diameter);
+
+		JPanel sliders = new JPanel();
+		sliders.setLayout(new BoxLayout(sliders, BoxLayout.LINE_AXIS));
+		altPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 0, 15));
+		diaPanel.setBorder(BorderFactory.createEmptyBorder(10, 15, 0, 10));
+		sliders.add(altPanel);
+		sliders.add(diaPanel);
+		
+		JPanel buttons = new JPanel();
+		buttons.setLayout(new BoxLayout(buttons, BoxLayout.LINE_AXIS));
+		buttons.add(cancel);
+		buttons.add(Box.createRigidArea(new Dimension(40,0)));
+		buttons.add(accept);
+		buttons.setBorder(BorderFactory.createEmptyBorder(20,100, 20, 10));
+
+		mainPane.add(sliders);
+		mainPane.add(buttons, BorderLayout.SOUTH);
+		
 		pack();
 		setVisible(true);
+		
+		// add the action listeners
+		altitude.addChangeListener(this);
+		diameter.addChangeListener(this);
+		accept.addActionListener(this);
+		cancel.addActionListener(this);
+		map.addMouseListener(this);
+		map.addMouseMotionListener(this);
+		
+		selecting = false;
 	}
 	
+	/**
+	 * add mountains to the specified area
+	 */
+	private void orogeny(double x0, double y0, double x1, double y1, double altitude, double diameter) {
+		System.out.println("create mountains from <" + x0 + "," + y0 + "> to <" + x1 + "," + y1 + "> w/A=" + altitude + ", D=" + diameter);
+	}
+	
+	private void redraw() {
+		double x_mid = parms.x_extent/2;
+		double y_mid = parms.y_extent/2;
+		int width = map.getWidth();
+		int height = map.getHeight();
+		
+		double X0 = x_start/width - x_mid;
+		double Y0 = y_start/height - y_mid;
+		double X1 = x_end/width - x_mid;
+		double Y1 = y_end/width - y_mid;
+		double A = altitude.getValue()/10000;
+		double D = diameter.getValue()/1000;
+		orogeny(X0, Y0, X1, Y1, A, D);
+	}
+	
+	/**
+	 * start defining a mountain range
+	 */
+	public void mousePressed(MouseEvent e) {
+		x_start = e.getX();
+		y_start = e.getY();
+		selecting = true;
+	}
+
+	/**
+	 * finish defining a mountain range
+	 */
+	public void mouseReleased(MouseEvent e) {
+		if (selecting) {
+			x_end = e.getX();
+			y_end = e.getY();
+			selecting = false;
+			redraw();
+		}
+		
+	}
+	
+	/**
+	 * progress in region selection
+	 */
+	public void mouseDragged(MouseEvent e) {
+		if (selecting) {
+			map.select(x_start,  y_start,  e.getX()-x_start,  e.getY()-y_start, Map.SEL_LINEAR);
+		}	
+	}
 	
 	/**
 	 * Window Close event handler ... implicit CANCEL
@@ -96,45 +174,20 @@ public class MountainDialog extends JFrame implements MouseMotionListener, Mouse
 	public void windowClosing(WindowEvent e) {
 		System.out.println("Closing Slope Dialog");
 		map.select(0, 0, 0, 0,  Map.SEL_NONE);
+		if (oldMesh != null) {
+			map.setMesh(oldMesh);
+			map.repaint();
+		}
 		this.dispose();
 	}
 	
 	/**
-	 * mouse-press ... start a selected area definition
-	 */
-	public void mousePressed(MouseEvent e) {
-		select_x = e.getX();
-		select_y = e.getY();
-		select_type = Map.SEL_LINEAR;
-	}
-
-	/**
-	 * mouse release ... create the mountains
-	 */
-	public void mouseReleased(MouseEvent arg0) {
-		select_type = Map.SEL_NONE;
-		map.select(0, 0, 0, 0, Map.SEL_NONE);
-		// TODO draw the mountains
-	}
-
-	/**
-	 * mouse drag ... update the selection line
-	 */
-	public void mouseDragged(MouseEvent e) {
-		int dx = e.getX() - select_x;
-		int dy = e.getY() - select_y;
-		map.select(select_x,  select_y, dx, dy, select_type);
-	}
-
-	/**
-	 * slider adjustments to altitude and diameter
+	 * updates to the axis/inclination sliders
 	 */
 	public void stateChanged(ChangeEvent e) {
-		if (e.getSource() == altitude) {
-			System.out.println("altitude = " + altitude.getValue());
-		} else if (e.getSource() == diameter) {
-			System.out.println("diameter = " + diameter.getValue());
-		}
+		if (e.getSource() == altitude || e.getSource() == diameter) {
+				redraw();
+		} 
 	}
 
 	/**
@@ -143,31 +196,25 @@ public class MountainDialog extends JFrame implements MouseMotionListener, Mouse
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == cancel) {
 			map.select(0,0,0,0, Map.SEL_NONE);
-			System.out.println("CANCEL slope");
+			map.setMesh(oldMesh);
+			map.repaint();
+			oldMesh = null;
 			this.dispose();
 		} else if (e.getSource() == accept) {
 			map.select(0,0,0,0, Map.SEL_NONE);
-			System.out.println("ACCEPT slope");
+			oldMesh = null;	// don't need this anymore
 			this.dispose();
 		}
 	}
 
+	public void mouseClicked(MouseEvent arg0) {}
+	public void mouseMoved(MouseEvent arg0) {}
+	public void mouseEntered(MouseEvent arg0) {}
+	public void mouseExited(MouseEvent arg0) {}
 	public void windowActivated(WindowEvent arg0) {}
 	public void windowClosed(WindowEvent arg0) {}
 	public void windowDeactivated(WindowEvent arg0) {}
 	public void windowDeiconified(WindowEvent arg0) {}
 	public void windowIconified(WindowEvent arg0) {}
 	public void windowOpened(WindowEvent arg0) {}
-
-
-	public void mouseClicked(MouseEvent arg0) {}
-	public void mouseEntered(MouseEvent arg0) {}
-	public void mouseExited(MouseEvent arg0) {}
-	public void mouseMoved(MouseEvent arg0) {}
-
-
-	
-
-
-	
 }

69b9738239c085e321792b55da0db992cc15af10
Mark Kampe <mark.kampe@gmail.com>
2017-07-23 09:13:03 -0700

    Add support for cartesian height map
    	interpolated from Voronoi points

--- src/worldBuilder/Map.java
+++ src/worldBuilder/Map.java
@@ -6,15 +6,17 @@ import javax.swing.*;
 
 public class Map extends JPanel {
 	/**
-	 * a Map is the displayable form of a Mesh.
+	 * a Map is the displayable resizeable form of a Mesh.
 	 * 
-	 *   it may include more continuous maps of height and 
-	 *   other attributes, but all of those are deterministic
-	 *   functions, computed from the Mesh.
-	 * 
-	 *   
-	 * The map window is resizable, and the mesh is unaware of the
-	 * display window size.
+	 * This class maintains a few basic types of information:
+	 * 	 1. a Mesh of random Voronoi MeshPoints (with attributes) 
+	 *   2. Cartesian arrays with the attributes of displayable points
+	 *   3. a translation ray to determine the values of Cartesian
+	 *      cells as a function of the surrounding Voronoi points.
+	 *      
+	 * This class operates on logical (-0.5 to +0.5) mesh coordinates,
+	 * and height/width display coordinates.  It is (largely) agnostic
+	 * about real-world (e.g. meters) coordinates.
 	 */
 
 	// types of displays
@@ -32,6 +34,7 @@ public class Map extends JPanel {
 	private static final int MIN_HEIGHT = 400;	// min screen height
 	private static final int SMALL_POINT = 2;	// width of a small point
 	private static final int LARGE_POINT = 4;	// width of a large point
+	private static final int TOPO_CELL = 4;		// width of a topographic cell
 	private Dimension size;
 
 	// display colors
@@ -46,13 +49,9 @@ public class Map extends JPanel {
 	private static final int TOPO_BRITE = 255;
 	private Color background;
 	
-	// the underlying collection of points and edges
-	private Mesh mesh;
-	
-	// the height map we use to generate topographical displays
-	private int heightMap[][];
-	private static final int UNKNOWN = 666666;
-	private double diminimus;	// minimum distance between separate points
+	// the interesting data
+	private Mesh mesh;				// mesh of Voronoi points
+	private MeshRef cartesian[][];	// Voronoi to Cartesian translation
 	
 	private Parameters parms;
 	
@@ -93,13 +92,34 @@ public class Map extends JPanel {
 	}
 
 	/**
-	 * set the mesh to be displayed
+	 * reset the mesh to be displayed
+	 * 		typically used to switch between a base Mesh
+	 * 		and a tentative mesh (e.g. for new slopes,
+	 * 		mountains, erosion, etc).
+	 * 
+	 * NOTE: this is a change of MeshPoints only.
+	 * 		 the voronoi vertex coordinates are assumed
+	 * 		 to be unchanged.
 	 */
 	public void setMesh(Mesh m) {
-		setVisible(false);
+		setVisible(false);	// avoid races w/paint
 		this.mesh = m;
 		setVisible(true);
 		repaint();
+	}
+	
+	/**
+	 * load an entirely new mesh
+	 * 		based on a new/different set of Voronoi vertices
+	 */
+	public void newMesh(Mesh m) {
+		setVisible(false);	// avoid races w/paint
+		this.mesh = m;
+		newMap();			// recreate Voronoi-to-Cartesian map
+		setVisible(true);
+		repaint();
+		
+		// FIX: need to call newMap() when window resizes
 	}
 	
 	/**
@@ -250,6 +270,8 @@ public class Map extends JPanel {
 			g.setColor(SELECT_COLOR);
 			g.drawRect(sel_x0, sel_y0, sel_width, sel_height);
 			break;
+		case NONE:
+			break;
 		}
 	}
 	
@@ -257,10 +279,21 @@ public class Map extends JPanel {
 	/**
 	 * render a topographical map
 	 * 
-	 *   create a cell resolution height map 
+	 *   1. create a dense height map
 	 */
 	private void paint_topo(Graphics g) {
 		
+		// for each cell in the displayable grid
+		for(int r = 0; r < cartesian.length; r++)
+			for(int c = 0; c < cartesian[0].length; c++) {
+				// interpolate height (from surrounding MeshPoints)
+				double z = cartesian[r][c].height();
+				
+				// shade a rectangle for that altitude
+				double shade = TOPO_DIM + ((z + parms.z_extent/2) * (TOPO_BRITE - TOPO_DIM));
+				g.setColor(new Color((int) shade, (int) shade, (int) shade));
+				g.drawRect(c * TOPO_CELL, r * TOPO_CELL, TOPO_CELL, TOPO_CELL);
+			}
 	}
 	
 	/**
@@ -275,5 +308,120 @@ public class Map extends JPanel {
 	 */
 	public Dimension getPreferredSize() {
 		return size;
+	}
+	
+	/**
+	 * create mapping from Voronoi mesh to map grid
+	 * 
+	 * 		The Voronoi mesh is deliberately sparse and
+	 * 		non-uniform, but we must eventually produce a
+	 * 		dense and uniform map.  This function creates
+	 * 		a map which lists, for each 2D map point, the
+	 * 		three nearest Voronoi points (and their distances).
+	 * 		This can be used to interpolate the value of 
+	 * 		any property of any point as a function of
+	 * 		the immediately surrounding Voronoi points.
+	 * 
+	 * 		This map only changes when the Mesh changes,
+	 * 		or when the display/export size changes.
+	 */
+	public void newMap() {
+		int h = getHeight()/TOPO_CELL;
+		int w = getWidth()/TOPO_CELL;
+		
+		cartesian = new MeshRef[h][w];
+		for(int r = 0; r < h; r++) {
+			double y = (double) r/h - parms.y_extent/2;
+			for(int c = 0; c < w; c++) {
+				double x =  (double)c/w - parms.x_extent/2;
+				MeshPoint m = new MeshPoint(x,y);
+				MeshRef ref = new MeshRef();
+				cartesian[r][c] = ref;
+				for(int v = 0; v < mesh.vertices.length; v++) {
+					MeshPoint p = mesh.vertices[v];
+					ref.consider(p.index, p.distance(m));
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Each cell of the Voronoi to grid map is represented
+	 * by a MeshRef, which describes the 3 nearest Voronoi
+	 * points.
+	 * 
+	 * We refer to the Voronoi points by index rather than
+	 * by MeshPoint because the latter change as a result
+	 * of editing operations, but the former change only
+	 * when a new Mesh is created.
+	 * 
+	 * This is a private class within Map because it needs
+	 * to make reference to the selected Mesh.
+	 */
+	private class MeshRef {
+		public int neighbors[];		// MeshPoint index
+		private double distances[];	// distance to that MeshPoint
+		private static final int NUM_NEIGHBORS = 3;
+		
+		public MeshRef() {
+			neighbors = new int[NUM_NEIGHBORS];
+			distances = new double[NUM_NEIGHBORS];
+			
+			for(int i = 0; i < NUM_NEIGHBORS; i++) {
+				neighbors[i] = -1;
+				distances[i] = 666;
+			}
+		}
+		
+		/**
+		 * consider a MeshPoint and note the three closest
+		 * 
+		 * @param index 	index of this mesh point
+		 * @param distance	distance to this mesh points
+		 */
+		public void consider(int index, double distance) {
+			if (distance >= distances[2])
+				return;
+			
+			// XXX there is a more elegant/general way to code this
+			if (distance >= distances[1]) {
+				// replace last in list
+				neighbors[2] = index;
+				distances[2] = distance;
+			} else if (distance >= distances[0]) {
+				// replace second in list
+				neighbors[2] = neighbors[1];
+				distances[2] = distances[1];
+				neighbors[1] = index;
+				distances[1] = distance;
+			} else {
+				// replace first in list
+				neighbors[2] = neighbors[1];
+				distances[2] = distances[1];
+				neighbors[1] = neighbors[0];
+				distances[1] = distances[0];
+				neighbors[0] = index;
+				distances[0] = distance;	
+			}
+		}
+		
+		/**
+		 * @return proximity weighted average height
+		 */
+		public double height() {
+			double z[] = new double[NUM_NEIGHBORS];
+			double d[] = new double[NUM_NEIGHBORS];
+			for(int n = 0; n < NUM_NEIGHBORS; n++) {
+				z[n] = mesh.vertices[neighbors[n]].z;
+				d[n] = distances[n];
+			}
+			
+			double zAvg = 0;
+			for(int n = 0; n < NUM_NEIGHBORS; n++) {
+				zAvg += z[n]/3;		// FIX: compute proximity weighted height
+			}
+			
+			return zAvg;
+		}
 	}
 }

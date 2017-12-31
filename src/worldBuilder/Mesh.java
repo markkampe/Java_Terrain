17b911659bcb26acfd6011b4395b9d8ae53cc739
Mark.Kampe <mark.kampe@gmail.com>
2017-12-30 21:59:31 -0800

    remove obsolete import

--- src/worldBuilder/Mesh.java
+++ src/worldBuilder/Mesh.java
@@ -3,7 +3,6 @@ package worldBuilder;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;

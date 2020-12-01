146ac2e1137d3ea702247f019d34503316e1e7fb
Mark.Kampe <mark.kampe@gmail.com>
2020-12-01 15:41:16 -0800

    WIP flora placement
    	FloraRule implements and reads sub-class definitions
    	FloraDialog loads and traverses them

--- src/worldBuilder/FloraDialog.java
+++ src/worldBuilder/FloraDialog.java
@@ -2,6 +2,8 @@ package worldBuilder;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ListIterator;
+
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
@@ -10,30 +12,43 @@ import javax.swing.event.ChangeListener;
  * Dialog to enable the creation a consistent map of a sub-region of the current world.
  */
 public class FloraDialog extends JFrame implements ActionListener, ChangeListener, MapListener, WindowListener {	
-	private final String FLORA_RULES = "flora.json";
-
+	private final String FLORA_RULES = "flora.json";	// FIX make FLORA_RULES a parameter w/default
+	private static final int MAX_TYPES = 20;	// max number of flora subtypes
+	private static String[] floraClasses = { "Tree", "Brush", "Grass" };
+	private boolean haveFloraTypes = false;
+	
 	private Map map;
 	private Parameters parms;
 	
+	// attribute maps
 	private double floraMap[];		// per mesh-point plant types
-	private double prevMap[];		// saved map
-	private Color floraColors[];	// type to flora map
+	private double prevFlora[];		// saved flora Map
+	private Color floraColors[];	// type to preview color map
+	private Color prevColors[];		// saved type to preview color map
+	private double hydroMap[];		// per mesh-point hydration
+	private double prevHydro[];		// saved hydro map
+	private double heightMap[];		// per mesh-point altitude
+	private double erodeMap[];		// per mesh-point erosion
+	private double soilMap[];		// per mesh-point soil map
 	
+	// widgets
 	private JButton accept;			// accept these updates
 	private JButton cancel;			// cancel dialog, no updates
 	private JTextField flora_palette;	// flora placement rules
 	private JButton chooseFlora;	// browse for flora placemen trulesnewColors
-	
 	private JSlider flora_pct;		// fraction of area to be covered
 	private RangeSlider flora_3;	// grass/brush/tree distribution
 	private JSlider goose_temp;		// goose temperature
 	private JSlider goose_hydro1;	// goose hydration adder
 	private JSlider goose_hydro2;	// goose hydration multiplier
 	
+	// selected region info
 	private boolean selected;		// a region has been selected
 	private double x0, y0;			// upper left hand corner
 	private double width, height;	// selected area size (in pixels)
+	private int placed[];			// count of per-type placements
 
+	// enablers for esoteric WIP attribute tweaks
 	private final boolean goose_t = false;
 	private final boolean goose_h1 = false;
 	private final boolean goose_h2 = false;
@@ -48,11 +63,28 @@ public class FloraDialog extends JFrame implements ActionListener, ChangeListene
 		this.map = map;
 		this.parms = Parameters.getInstance();
 		
-		// save and copy the incoming flora map
-		prevMap = map.getFloraMap();
-		floraMap = new double[prevMap.length];
-		for(int i = 0; i < floraMap.length; i++)
-			floraMap[i] = prevMap[i];
+		// get incoming maps, and copy the hydration/flora maps
+		heightMap = map.getHeightMap();
+		erodeMap = map.getErodeMap();
+		soilMap = map.getSoilMap();
+		prevFlora = map.getFloraMap();
+		prevHydro = map.getHydrationMap();
+		floraMap = new double[prevFlora.length];
+		hydroMap = new double[prevHydro.length];
+		for(int i = 0; i < floraMap.length; i++) {
+			floraMap[i] = prevFlora[i];
+			hydroMap[i] = prevHydro[i];
+		}
+		
+		// get and copy the type to preview color map
+		floraColors = new Color[MAX_TYPES];
+		prevColors = map.getFloraColors();
+		if (prevColors != null)
+			for(int i = 0; i < floraColors.length; i++)
+				floraColors[i] = prevColors[i];
+		
+		// initial placement counts
+		placed = new int[MAX_TYPES];
 		
 		// create the dialog box
 		Container mainPane = getContentPane();
@@ -155,13 +187,7 @@ public class FloraDialog extends JFrame implements ActionListener, ChangeListene
 			goose_hydro1.setFont(fontSmall);
 			goose_hydro1.setPaintTicks(true);
 			goose_hydro1.setPaintLabels(true);
-			fTitle = new JLabel("Hydration plus/minus (percentag// save and copy the incoming flora map\n" + 
-					"		prevMap = map.getFloraMap();\n" + 
-					"		System.err.println(\"prevMap.length = \" + prevMap.length);\n" + 
-					"		floraMap = new double[prevMap.length];\n" + 
-					"		for(int i = 0; i < floraMap.length; i++)\n" + 
-					"			floraMap[i] = prevMap[i];\n" + 
-					"		System.err.println(\"floraMap.length = \" + floraMap.length);e)");
+			fTitle = new JLabel("Hydration plus/minus (percentage)");
 			fTitle.setFont(fontSmall);
 	
 			locals.add(new JLabel("    "));
@@ -215,26 +241,108 @@ public class FloraDialog extends JFrame implements ActionListener, ChangeListene
 	}
 	
 	/**
-	 * re-determine the plant coverage of each mesh point
+	 * update the soil hydration for each selected mesh point
 	 */
-	void placeFlora() {
-		floraColors = new Color[2];
-		floraColors[0] = Color.BLACK;
-		floraColors[1] = Color.GREEN;
+	void updateHydro() {
+		double plus = goose_hydro1.getValue();	// percentage
+		double scale = goose_hydro2.getValue();	// percentage
 		
 		MeshPoint[] points = map.mesh.vertices;
+		for(int i = 0; i < hydroMap.length; i++) {
+			// make sure it is in selected area
+			if (points[i].x < x0 || points[i].x >= x0+width ||
+				points[i].y < y0 || points[i].y >= y0+height)
+				continue;
+			
+			// make sure it is a land point
+			double h = prevHydro[i];
+			if (h < 0)
+				continue;
+			
+			// compute the updated hydration for this point
+			h *= scale/100.0;
+			h += plus/100.0;
+			if (h < 0)
+				h = 0;
+			if (h > 0.99)
+				h = 0.99;
+			
+			hydroMap[i] = h;
+		}
+	}
+	
+	/**
+	 * re-determine the plant coverage of each selected mesh point
+	 */
+	private int[] placeFlora() {
+		// allocate an array for the sub-type colors
+		floraColors = new Color[MAX_TYPES];
+		floraColors[0] = Color.BLACK;
+		
+		// make sure we have rules
+		if (!haveFloraTypes) {
+			FloraRule.loadFlora(FLORA_RULES);
+			haveFloraTypes = true;
+		}	
+		
+		// figure out how many points we have to populate
+		int point_count = 0;
+		MeshPoint[] points = map.mesh.vertices;
+		for(int i = 0; i < floraMap.length; i++)
+			if (points[i].x >= x0 && points[i].x < x0+width &&
+				points[i].y >= y0 && points[i].y < y0+height)
+				point_count++;
+
+		// figure out the per-class quotas
+		int quotas[] = new int[3];
+		quotas[0] = (point_count * (100 - flora_3.getUpperValue()))/100;
+		quotas[2] = (point_count * flora_3.getValue())/100;
+		quotas[1] = point_count = (quotas[0] + quotas[2]);
+		
+		// get the list of flora sub-types
+		FloraRule bidders[] = new FloraRule[MAX_TYPES];
+		int numRules = 0;
+		for( ListIterator<FloraRule> it = FloraRule.iterator(); it.hasNext();) {
+			FloraRule r = it.next();
+			bidders[numRules++] = r;
+			floraColors[numRules] = r.previewColor;
+		}
+		Bidder bidder = new Bidder(numRules);	// bid manager
+		
+		// get the sea-level temperature range
+		double Twinter = parms.meanWinter();
+		double Tsummer = parms.meanSummer();
+		
+		// let every sub-class bid on each selected mesh point
 		for(int i = 0; i < floraMap.length; i++) {
 			// make sure it is in selected area
 			if (points[i].x < x0 || points[i].x >= x0+width ||
 				points[i].y < y0 || points[i].y >= y0+height)
 				continue;
-			floraMap[i] = ((i%2) == 1) ? 1 : 0;
-			System.out.println("   " + i + "<-" + floraMap[i]);
+			
+			// gather per-point bidding attributes
+			int alt = (int) parms.altitude(heightMap[i] - erodeMap[i]);
+			double lapse = alt * parms.lapse_rate;
+			double hydro = hydroMap[i];
+			double soil = soilMap[i];
+			
+			// find the first sub-class that bids on it
+			for(int r = 0; r < numRules; r++) {
+				double bid = bidders[r].bid(alt, hydro, Twinter - lapse, Tsummer - lapse, soil);
+				if (bid > 0) {
+					floraMap[i] = r + 1;
+					placed[r+1]++;
+					break;
+				}
+			}
 		}
-		
+	
+		// instantiate (and display) the updated flora map
 		map.setFloraColors(floraColors);
 		map.setFloraMap(floraMap);
 		map.repaint();
+		
+		return placed;
 	}
 
 	/**
@@ -266,9 +374,10 @@ public class FloraDialog extends JFrame implements ActionListener, ChangeListene
 		
 		if ((source == flora_pct || source == flora_3) && selected)
 			placeFlora();
-		else if ((source == goose_hydro1 || source == goose_hydro2) && selected)
+		else if ((source == goose_hydro1 || source == goose_hydro2) && selected) {
+			updateHydro();
 			placeFlora();
-		else if ((source == goose_temp) && selected)
+		} else if ((source == goose_temp) && selected)
 			placeFlora();
 	}
 	
@@ -294,8 +403,15 @@ public class FloraDialog extends JFrame implements ActionListener, ChangeListene
 	 */
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == accept && selected) {
-			if (parms.debug_level > 0)
-				System.out.println("LOG FLORAL CHANGES");
+			if (parms.debug_level > 0) {
+				System.out.println("Flora Placement (" + FloraRule.ruleset + "):");
+				int number = 1;
+				for( ListIterator<FloraRule> it = FloraRule.iterator(); it.hasNext();) {
+					FloraRule r = it.next();
+					System.out.println("    " + r.className + "." + r.ruleName + ":\t" + placed[number]);
+					number++;
+				}
+			}
 		} else if (e.getSource() == chooseFlora) {
 			FileDialog d = new FileDialog(this, "Floral Palette", FileDialog.LOAD);
 			d.setFile(flora_palette.getText());
@@ -307,8 +423,11 @@ public class FloraDialog extends JFrame implements ActionListener, ChangeListene
 					palette_file = dir + palette_file;
 				flora_palette.setText(palette_file);
 			}
+			haveFloraTypes = false;
 		} else if (e.getSource() == cancel) {
-			map.setFloraMap(prevMap);
+			map.setFloraColors(prevColors);
+			map.setFloraMap(prevFlora);
+			map.repaint();
 			cancelDialog();
 		}
 	}
---
+++ src/worldBuilder/FloraRule.java
@@ -0,0 +1,442 @@
+package worldBuilder;
+
+import java.awt.Color;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+import javax.json.Json;
+import javax.json.stream.JsonParser;
+
+
+/**
+ * parameters and bids for a single Flora sub-type
+ */
+public class FloraRule {
+	private static final String DEFAULT_CONFIG = "/Templates";
+	private static final int NO_VALUE = 666666;
+	private static final int RULE_DEBUG = 2;
+	
+	/** list of all ingested rules	*/
+	public static String ruleset;
+	private static LinkedList<FloraRule> rules;
+	
+	/** name of this rule and its class	*/
+	public String ruleName, className;
+
+	/** Altitude ranges for this rule	*/
+	public int minAltitude, maxAltitude;
+	/** Hydration and water-depth ranges for this rule	*/
+	public double minDepth, maxDepth, minHydro, maxHydro;
+	/** Temperature range for this rule	*/
+	public double minTemp, maxTemp;
+	/** soil type range for this rule	*/
+	public double minSoil, maxSoil;
+	/** is there flexibility in these ranges	*/
+	public boolean flexRange;
+	/** taper bids as we move away from mid-range	*/
+	public boolean taperedBid;
+	
+	/** how high should this rule bid	*/
+	public int vigor;
+	
+	/** what color should this render as in previews	*/
+	public Color previewColor;
+	
+	/** Debug: trace bids from this rule	*/
+	public boolean debug;			// trace bids from this rule
+
+	/** Debug: explain the basis for the last bid	*/
+	public String justification;
+	
+	private static Parameters parms = Parameters.getInstance();
+	private static final double IMPOSSIBLE = -666.0;
+	
+	/**
+	 * create a new subtype
+	 * @param name of this subtype
+	 */
+	public FloraRule(String name) {
+		this.ruleName = name;
+		this.debug = false;
+		previewColor = null;
+		
+		// default values will meet any terrestrial conditions
+		className = null;
+		minAltitude = -parms.alt_max;
+		maxAltitude = parms.alt_max;
+		minDepth = 0;
+		maxDepth = 0;
+		minHydro = 0.0;
+		maxHydro = 1.0;
+		minTemp = -60;
+		maxTemp = 70;
+		minSoil = 0;
+		maxSoil = Map.ALLUVIAL + 1;
+		flexRange = false;
+		taperedBid = false;
+		vigor = 16;
+	}
+	
+	// load and iterate over Flora type rules
+	public static int size() { return rules.size(); }
+	public static ListIterator<FloraRule> iterator() { return rules.listIterator(); }
+	
+	/**
+	 * load in Flora sub-type definitions
+	 * @param file name of file to be read
+	 */
+	public static void loadFlora(String file) {
+		rules = new LinkedList<FloraRule>();
+		
+		Parameters parms = Parameters.getInstance();
+		BufferedReader r;
+		JsonParser parser;
+		String filename;
+		if (file.charAt(0) != '/') {
+			filename = DEFAULT_CONFIG + "/" + file;
+			InputStream s = FloraRule.class.getResourceAsStream(filename);
+			r = new BufferedReader(new InputStreamReader(s));
+		} else {
+			filename = file;
+			try {
+				r = new BufferedReader(new FileReader(filename));
+			} catch (FileNotFoundException e) {
+				System.out.println("ERROR: unable to open flora types file " + filename);
+				return;
+			}
+		}
+		
+		// parsing state variables
+		FloraRule thisRule = null;
+		String name = "", thisKey = "", thisObject = "", thisValue = "";
+		boolean inRules = false;
+		
+		int    aMin = NO_VALUE, aMax = NO_VALUE;	// altitude
+		double dMin = NO_VALUE, dMax = NO_VALUE;	// depth
+		double hMin = NO_VALUE, hMax = NO_VALUE;	// hydration
+		double tMin = NO_VALUE, tMax = NO_VALUE;	// temperature
+		double sMin = NO_VALUE, sMax = NO_VALUE;	// soil
+		int red = NO_VALUE, blue = NO_VALUE, green = NO_VALUE;	
+		int vigor = NO_VALUE;
+
+		boolean flexRange = false;
+		boolean taperedBid = false;
+		
+		String className = null;					// floral class
+		
+		int inColor = 0;
+		
+		parser = Json.createParser(r);
+		while (parser.hasNext()) {
+			JsonParser.Event e = parser.next();
+			switch (e) {
+			case KEY_NAME:
+				thisKey = parser.getString();
+				break;
+				
+			case START_OBJECT:
+				if (thisKey != "") {
+					thisObject = thisKey;
+				}
+				break;
+				
+			case END_OBJECT:
+				if (!inRules)
+					break;
+				
+				// are these min/max attributes
+				if (thisObject == "") {	// end of a new rule?
+					// no name or illegal level is not a real rule
+					if (name.equals(""))
+						break;
+					
+					// create a new rule
+					thisRule = new FloraRule(name);
+					
+					// copy in all of the values we got
+					if (className != null)
+						thisRule.className = className;
+					if (aMin != NO_VALUE)
+						thisRule.minAltitude = aMin;
+					if (hMin != NO_VALUE)
+						thisRule.minHydro = hMin;
+					if (dMin != NO_VALUE)
+						thisRule.minDepth = dMin;
+					if (tMin != NO_VALUE)
+						thisRule.minTemp = tMin;
+					if (sMin != NO_VALUE)
+						thisRule.minSoil = sMin;
+					if (aMax != NO_VALUE)
+						thisRule.maxAltitude = aMax;
+					if (hMax != NO_VALUE)
+						thisRule.maxHydro = hMax;
+					if (dMax != NO_VALUE)
+						thisRule.maxDepth = dMax;
+					if (tMax != NO_VALUE)
+						thisRule.maxTemp = tMax;
+					if (sMax != NO_VALUE)
+						thisRule.maxSoil = sMax;
+					if (vigor != NO_VALUE)
+						thisRule.vigor = vigor;
+					if (red != NO_VALUE)
+						thisRule.previewColor = new Color(red, blue, green);
+					thisRule.flexRange = flexRange;
+					thisRule.taperedBid = taperedBid;
+					rules.add(thisRule);
+					
+					// now reset all System.out.println("read string for key " + thisKey);the parameters for the next rule
+					aMin = NO_VALUE; aMax = NO_VALUE;
+					dMin = NO_VALUE; dMax = NO_VALUE;
+					hMin = NO_VALUE; hMax = NO_VALUE;
+					tMin = NO_VALUE; tMax = NO_VALUE;
+					flexRange = false;
+					taperedBid = false;
+					vigor= NO_VALUE;
+					red = NO_VALUE; green = NO_VALUE; blue = NO_VALUE;
+					name = "";
+				} else
+					thisObject = "";
+				thisKey = "";
+				break;
+				
+			case START_ARRAY:
+				if (thisKey.equals("color")) {
+						inColor = 1;
+				} else {
+					if (!thisKey.equals("")) {
+						ruleset = thisKey;
+						thisKey = "";
+					}
+					inRules = true;
+				}
+				break;
+				
+			case END_ARRAY:
+				thisKey = "";
+				if (inColor > 0)
+					inColor = 0;
+				else
+					inRules = false;
+				break;
+
+			case VALUE_STRING:
+				switch (thisKey) {
+				case "//":
+					parser.getString();
+					break;
+				case "name":
+					name = parser.getString();
+					break;
+				case "class":
+					className = parser.getString();
+					break;
+				case "range":
+					flexRange = parser.getString().equals("flexible");
+					break;
+				case "bid":
+					taperedBid = parser.getString().equals("tapered");
+					break;
+				}
+				thisKey = "";
+				break;
+
+			case VALUE_NUMBER:
+				if (inColor > 0) {
+					switch(inColor) {
+					case 1:
+						red = parser.getInt();
+						inColor++;
+						break;
+					case 2:
+						blue = parser.getInt();
+						inColor++;
+						break;
+					case 3:
+						green = parser.getInt();
+						inColor++;
+						break;
+					default:
+						System.err.println("ERROR: too many fields in color");
+						break;
+					}
+					break;
+				}
+				
+				switch (thisKey) {
+				case "vigor":
+					vigor = parser.getInt();
+					thisKey = "";
+					break;
+				case "min":
+					thisValue = parser.getString();
+					switch(thisObject) {
+					case "alt":
+						aMin = Integer.parseInt(thisValue);
+						break;
+					case "temp":
+						tMin = Double.parseDouble(thisValue);
+						break;
+					case "hydro":
+						hMin = Double.parseDouble(thisValue);
+						break;
+					case "depth":
+						dMin = Double.parseDouble(thisValue);
+						break;
+					case "soil":
+						sMin = Double.parseDouble(thisValue);
+						break;
+					}
+					thisKey = "";
+					break;
+					
+				case "max":
+					thisValue = parser.getString();
+					switch(thisObject) {
+					case "alt":
+						aMax = Integer.parseInt(thisValue);
+						break;
+					case "temp":
+						tMax = Double.parseDouble(thisValue);
+						break;
+					case "hydro":
+						hMax = Double.parseDouble(thisValue);
+						break;
+					case "depth":
+						dMax = Double.parseDouble(thisValue);
+						break;
+					case "soil":
+						sMax = Double.parseDouble(thisValue);
+						break;
+					}
+					thisKey = "";
+					break;
+				}
+				break;
+
+			default:
+				break;
+			}
+		}
+		
+		// see if we have been asked for debug output
+		if (parms.debug_level >= RULE_DEBUG) {
+			System.out.println("Flora rules (" + ruleset + ") from " + filename + ":");
+			for(ListIterator<FloraRule> it = iterator(); it.hasNext(); ) {
+				it.next().dump("    ");
+			}
+		}
+	}
+	
+	
+	/**
+	 * dump out a Flora sub-type (for debugging)
+	 * @param prefix ... leading blanks
+	 */
+	public void dump( String prefix ) {
+		System.out.print(prefix + "subtype: " + ruleName);
+		System.out.println("");
+		if (className != null)
+			System.out.println(prefix + "      " + "class:   " + className);
+		System.out.println(prefix + "      " + "alt:     " + minAltitude + "-" + maxAltitude);
+		System.out.println(prefix + "      " + "depth:   " + (int) minDepth + "-" + (int) maxDepth);
+		System.out.println(prefix + "      " + "hydro:   " + String.format("%.1f", minHydro) + "-" + String.format("%.1f", maxHydro));
+		System.out.println(prefix + "      " + "temp:    " + minTemp + "-" + maxTemp);
+		System.out.println(prefix + "      " + "soil:    " + String.format("%.1f", minSoil) + "-" + String.format("%.1f", maxSoil));
+		System.out.println(prefix + "      " + "range:   " + (flexRange ? "flexible" : "strict"));
+		System.out.println(prefix + "      " + "bid:     " + (taperedBid ? "tapered" : "flat"));
+		if (previewColor != null)
+			System.out.println(prefix + "      " + "color:Flora   " +
+								previewColor.getRed() + "," +
+								previewColor.getGreen() + "," +
+								previewColor.getBlue());
+		System.out.println(prefix + "      " + "vigor:   " + vigor);
+	}
+	
+	/**
+	 * compute how much to bid based on value and my range
+	 * 
+	 * @param value		actual value
+	 * @param min		bottom of acceptable range
+	 * @param max		top of acceptable range
+	 * 
+	 * @return			0 to 1 for a favorable bid
+	 * 					0 to -1 for an unfavorable bid
+	 * 					IMPOSSIBLE if we are outside the range
+	 * note:
+	 *		taperedBid: bid is proportional to place in range
+	 *		flexRange: negative bids for barely outside range
+	 */
+	double range_bid(double value, double min, double max) {
+		
+		// figure out the acceptable range and where we are in it
+		double mid = (min + max)/2;
+		double range = mid - min;
+		double delta = Math.abs(value - mid);
+		
+		// if within range, bid based on distance to center
+		if (delta <= range)
+			return taperedBid ? 1.0 - delta/range : 1.0;
+		
+		// if outside worst acceptable we fail
+		if (!flexRange || delta > 2*range)
+			return IMPOSSIBLE;
+		
+		// return negative based on how far off we are
+		return taperedBid ? -(delta - range)/range : -1.0;
+	}
+	
+	/**
+	 * compute the bid that this tile will make for a grid square
+	 * 		total bid is the sum of characteristic bids
+	 * 		characteristic bid is based on where it is in range
+	 * 
+	 * @param alt		altitude(M)
+	 * @param hydro		hydration(%)
+	 * @param winter	low temp(degC)
+	 * @param summer	high temp(degC)
+	 * @param soil		soil type
+	 * 
+	 * @return			bid
+	 */
+	double bid(double alt, double hydro, double winter, double summer, double soil) {
+
+		// range check vs altitude, hydration and temperature
+		double score = 0;
+		double v;
+		justification = "";
+		v = range_bid(alt, minAltitude, maxAltitude);
+		if (v <= 0)
+			justification += "alt";
+		score += v;
+		
+		if (hydro >= 0) {
+			v = range_bid(hydro, minHydro, maxHydro);
+			if (v <= 0)
+				justification += "+hydro";
+		}
+		
+		v = range_bid((winter+summer)/2, minTemp, maxTemp);
+		if (v <= 0)
+			justification += "+temp";
+		score += v;
+		
+		v = range_bid(soil, minSoil, maxSoil);
+		if (v <= 0)
+			justification += "+soil";
+		score += v;
+		return vigor * score;
+	}
+	
+	/**
+	 * is this rule inapplicable to a particular floral class
+	 * @param  floraClass to be checked
+	 */
+	public boolean wrongFlora(String floraClass) {
+		return (className != null && !className.equals(floraClass));
+	}
+}

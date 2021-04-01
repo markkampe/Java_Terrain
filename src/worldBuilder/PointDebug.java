68b038ca928825149359390fb5a231ec2d4d6af0
Mark.Kampe <mark.kampe@gmail.com>
2021-03-31 17:35:55 -0700

    WIP - adding map point (vs MeshPoint) info

--- src/worldBuilder/PointDebug.java
+++ src/worldBuilder/PointDebug.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 /**
  * a JFrame that displays all the infomration about a selected mesh-point
  */
-public class PointDebug extends JFrame implements WindowListener, MapListener {
+public class PointDebug extends JFrame implements WindowListener, MapListener, ActionListener {
 		private Map map;
 		private Parameters parms;
 			
@@ -18,20 +18,30 @@ public class PointDebug extends JFrame implements WindowListener, MapListener {
 		private static final long serialVersionUID = 1L;
 		
 		private JLabel infoIndex;
+		private JLabel labelIndex;
 		private JLabel infoMap;
 		private JLabel infoWorld;
 		private JLabel infoAlt;
 		private JLabel infoFlux;
+		private JLabel labelFlux;
 		private JLabel infoVelocity;
+		private JLabel labelVelocity;
 		private JLabel infoRain;
 		private JLabel infoErode;
 		private JLabel infoSuspended;
+		private JLabel labelSuspended;
 		private JLabel infoSoil;
 		private JLabel infoHydro;
 		private JLabel infoFlora;
 		private JLabel infoDownhill;
+		private JLabel labelDownhill;
 		private JLabel infoOutlet;
+		private JLabel labelOutlet;
 		private JLabel infoNeighbors;
+		private JButton selectMode;
+		
+		private boolean meshMode = true;
+		private String modes[] = { "Map Point", "Mesh Point" };
 		
 		/** instantiate the point information display, register for selection events */
 		public PointDebug(Map map)  {
@@ -46,6 +56,7 @@ public class PointDebug extends JFrame implements WindowListener, MapListener {
 			addWindowListener( this );
 			setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
 			
+			selectMode = new JButton(modes[1]);
 			infoIndex = new JLabel("Select a point");
 			infoMap = new JLabel();
 			infoWorld = new JLabel();
@@ -61,11 +72,14 @@ public class PointDebug extends JFrame implements WindowListener, MapListener {
 			infoDownhill = new JLabel();
 			infoOutlet = new JLabel();
 			infoNeighbors = new JLabel();
-			int fields = 15;
+			int fields = 16;
 			
-			JPanel info = new JPanel(new GridLayout(fields,2));
+			JPanel info = new JPanel(new GridLayout(fields, 2));
 			info.setBorder(BorderFactory.createEmptyBorder(20,10,20,10));
-			info.add(new JLabel("Index:"));
+			info.add(new JLabel("Selection Mode:"));
+			info.add(selectMode);
+			labelIndex = new JLabel("Index:");
+			info.add(labelIndex);
 			info.add(infoIndex);
 			info.add(new JLabel("Map Location:"));
 			info.add(infoMap);
@@ -75,13 +89,16 @@ public class PointDebug extends JFrame implements WindowListener, MapListener {
 			info.add(infoAlt);
 			info.add(new JLabel("rainfall:"));
 			info.add(infoRain);
-			info.add(new JLabel("Water Flux:"));
+			labelFlux = new JLabel("Water Flux:");
+			info.add(labelFlux);
 			info.add(infoFlux);
-			info.add(new JLabel("Water Velocity:"));
+			labelVelocity = new JLabel("Water Velocity");
+			info.add(labelVelocity);
 			info.add(infoVelocity);
 			info.add(new JLabel("Erosion/(Deposition):"));
 			info.add(infoErode);
-			info.add(new JLabel("Suspended:"));
+			labelSuspended = new JLabel("Suspended:");
+			info.add(labelSuspended);
 			info.add(infoSuspended);
 			info.add(new JLabel("Flora type:"));
 			info.add(infoFlora);
@@ -89,9 +106,11 @@ public class PointDebug extends JFrame implements WindowListener, MapListener {
 			info.add(infoSoil);
 			info.add(new JLabel("Depth:"));
 			info.add(infoHydro);
-			info.add(new JLabel("Down-hill:"));
+			labelDownhill = new JLabel("Downhill:");
+			info.add(labelDownhill);
 			info.add(infoDownhill);
-			info.add(new JLabel("Outlet:"));
+			labelOutlet = new JLabel("Outlet:");
+			info.add(labelOutlet);
 			info.add(infoOutlet);
 			info.add(new JLabel("Neighbors: "));
 			info.add(infoNeighbors);
@@ -102,19 +121,56 @@ public class PointDebug extends JFrame implements WindowListener, MapListener {
 			setVisible(true);
 			
 			// add the action listeners
+			selectMode.addActionListener(this);
 			map.addMapListener(this);
 			map.selectMode(Map.Selection.POINT);
 			map.checkSelection(Map.Selection.POINT);
 		}
 		
 		/**
-		 * called when a point is selected on the map
-		 * @param map_x		(map coordinate)
-		 * @param map_y		(map coordinate)
+		 * called whenever a point is selected on the map
+		 * 
+		 * @param map_x	(map) x coordinate of click
+		 * @param map_y	(map) y coordinate of click
 		 */
 		public boolean pointSelected(double map_x, double map_y) {
+			// cancel all previous highlights
+			map.highlight(-1,  null);
 			
-			MeshPoint point = map.mesh.choosePoint(map_x, map_y);
+			// display the info for this point
+			if (meshMode) {
+				MeshPoint point = map.mesh.choosePoint(map_x, map_y);
+				meshPointInfo(point);
+				
+				// highlight selected point
+				map.highlight(point.index, SELECTED_COLOR);
+			} else {
+				mapPointInfo(map_x, map_y);
+			}
+			
+			// find and highlight our neighbors
+			Vicinity vicinity = new Polygon(map.mesh, map_x, map_y);
+			String neighbors = "";
+			for(int i = 0; i < Vicinity.NUM_NEIGHBORS; i++)
+				if (vicinity.neighbors[i] >= 0) {
+					map.highlight(vicinity.neighbors[i], NEIGHBOR_COLOR);
+					if (neighbors == "")
+						neighbors = String.format("%d", vicinity.neighbors[i]);
+					else
+						neighbors += String.format(", %d", vicinity.neighbors[i]);
+				}
+			infoNeighbors.setText(neighbors);
+			
+			// update the point information pop-up and display the highlights
+			pack();
+			map.repaint();
+			return true;
+		}
+		
+		/**
+		 * display info for a MeshPoint
+		 */
+		private void meshPointInfo(MeshPoint point) {
 			
 			if (point.immutable)
 				infoIndex.setText(String.format("%d (immutable)", point.index));
@@ -161,8 +217,9 @@ public class PointDebug extends JFrame implements WindowListener, MapListener {
 			
 			double depthMap[] = map.getDepthMap();
 			h = depthMap[point.index];
-			if (h >= 0)
-				desc = String.format("%.1f%s above outlet", h, Parameters.unit_z);
+			if (h > 0)
+				desc = String.format((h >= 10.0) ? "%.1f" : "%.2f", h) +
+						Parameters.unit_z + " above outlet";
 			else
 				desc = String.format((h > -10.0) ? "%.2f" : "%.1f", -h) +
 						Parameters.unit_z + " below water";
@@ -174,30 +231,92 @@ public class PointDebug extends JFrame implements WindowListener, MapListener {
 			double outlet = map.hydro.outlet[point.index];
 			infoOutlet.setText(outlet == Hydrology.UNKNOWN ? "NONE" :
 								String.format("%.1fMSL", parms.altitude(outlet)));
-		
-			// highlight selected point
-			map.highlight(-1, null);
-			map.highlight(point.index, SELECTED_COLOR);
-			
-			// find and highlight our neighbors
-			Vicinity vicinity = new Polygon(map.mesh, map_x, map_y);
-			String neighbors = "";
-			for(int i = 0; i < Vicinity.NUM_NEIGHBORS; i++)
-				if (vicinity.neighbors[i] >= 0) {
-					map.highlight(vicinity.neighbors[i], NEIGHBOR_COLOR);
-					if (neighbors == "")
-						neighbors = String.format("%d", vicinity.neighbors[i]);
-					else
-						neighbors += String.format(", %d", vicinity.neighbors[i]);
-				}
-			infoNeighbors.setText(neighbors);
-			
-			// update the point information pop-up and display the highlights
-			pack();
-			map.repaint();
-			return(true);
 		}
 		
+		/**
+		 * display information about a random map point
+		 * @param map_x
+		 * @param map_y
+		 */
+		void mapPointInfo(double map_x, double map_y) {
+			
+			infoMap.setText(String.format("<%.7f, %.7f>", map_x, map_y));
+			infoWorld.setText(String.format("<%.6f, %.6f>", parms.latitude(map_y), parms.longitude(map_x)));
+			
+			double h = 0; // = parms.height(erodeMap[point.index]);
+			String desc = "TBD";
+			
+			double heightMap[] = map.getHeightMap();
+			//infoAlt.setText(String.format("%.1f%s MSL", parms.altitude(heightMap[point.index]), Parameters.unit_z));
+			infoAlt.setText(desc);
+			
+			double rainMap[] = map.getRainMap();
+			//infoRain.setText(String.format("%.1f%s", rainMap[point.index], Parameters.unit_r));
+			infoRain.setText(desc);
+			
+			double erodeMap[] = map.getErodeMap();
+
+			if (h < 0)
+				desc = String.format("(%.3f%s)",- h, Parameters.unit_z);
+			else
+				desc = String.format("%.3f%s", h, Parameters.unit_z);
+			infoErode.setText(desc);
+			
+			double floraMap[] = map.getFloraMap();
+			//if (floraMap != null && floraMap[point.index] > 0)
+			//	infoFlora.setText(map.floraNames[(int) floraMap[point.index]]);
+			//else
+			// infoFlora.setText("None");
+			infoFlora.setText(desc);
+			
+			double soilMap[] = map.getSoilMap();
+			//desc = erodeMap[point.index] < 0 ? map.getSoilType("Alluvial") + "/" : "";
+			//infoSoil.setText(desc + map.rockNames[(int) soilMap[point.index]]);
+			infoSoil.setText(desc);
+			
+			double depthMap[] = map.getDepthMap();
+			//h = depthMap[point.index];
+			if (h > 0)
+				desc = String.format((h >= 10.0) ? "%.1f" : "%.2f", h) +
+						Parameters.unit_z + " above outlet";
+			else
+				desc = String.format((h > -10.0) ? "%.2f" : "%.1f", -h) +
+						Parameters.unit_z + " below water";
+			infoHydro.setText(desc);
+		}
+		
+		/**
+		 * respond to changes in the selectMode button
+		 */
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource() == selectMode) {
+				if (meshMode) {
+					infoIndex.setText("");
+					labelIndex.setForeground(Color.GRAY);
+					infoFlux.setText("");
+					labelFlux.setForeground(Color.GRAY);
+					infoVelocity.setText("");
+					labelVelocity.setForeground(Color.GRAY);
+					infoSuspended.setText("");
+					labelSuspended.setForeground(Color.GRAY);
+					infoDownhill.setText("");
+					labelDownhill.setForeground(Color.GRAY);
+					infoOutlet.setText("");
+					labelOutlet.setForeground(Color.GRAY);
+					selectMode.setText(modes[0]);
+					meshMode = false;
+				} else {
+					labelIndex.setForeground(Color.BLACK);
+					labelFlux.setForeground(Color.BLACK);
+					labelVelocity.setForeground(Color.BLACK);
+					labelSuspended.setForeground(Color.BLACK);
+					labelDownhill.setForeground(Color.BLACK);
+					labelOutlet.setForeground(Color.BLACK);
+					selectMode.setText(modes[1]);
+					meshMode = true;
+				}
+			}
+		}
 		/**
 		 * Window Close event handler ... do nothing
 		 */
